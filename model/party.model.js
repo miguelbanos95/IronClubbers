d7123d7996374b8e31625dc1780dea74002158fa
Pedro Alberto Baños Folcra <pedroalbertobanosfolcra@MacBook-Pro-de-Pedro-2.local>
2022-02-21 20:09:30 +0100

    seeds & model done

---
+++ model/party.model.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const partySchema = new Schema({
+    
+    name: {
+        type: String,
+        required: [true, "Name is required"]
+    },
+    address: {
+        type: String,
+        require:[true, "Address is required"]
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    image: {
+        type: String,
+        default: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.deviantart.com%2Fdonjas%2Fart%2FParty-People-215748549&psig=AOvVaw2IjpmC600t25AODVn0CMwU&ust=1645553982281000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPDS4Lm0kfYCFQAAAAAdAAAAABAJ",
+      },
+      description: {
+        type: String,
+        minlength: [10, 'Please enter at least 10 characters ']
+      },
+      categories: {
+        type: [String],
+        default: ["Techno"]
+      },
+      capacity: {
+        type: Number,
+        required: [true, 'What about capaciy, does not mean anuthing for you?' ]
+      }
+    }, { timestamps: true })
+
+    const Party = mongoose.model('party', partySchema)
+    module.exports = Party
+
